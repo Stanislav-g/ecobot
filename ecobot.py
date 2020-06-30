e294e7ae7d5c15c96bd6576ec56ffa13bb9f21bb
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-06-30 13:12:24 +0200

    Update ecobot.py

--- ecobot.py
+++ ecobot.py
@@ -382,8 +382,7 @@ async def perevod(ctx, member: discord.Member = None, amount: int = None):
 @client.command()
 async def w(ctx, author, *args):
     await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
-    num == len(args)
-    cursor.execute("UPDATE users SET cash = cash + {} WHERE id = {}".format(num, ctx.author.id))
+    cursor.execute("UPDATE users SET cash = cash + {} WHERE id = {}".format(len(args), ctx.author.id))
         
             
              
