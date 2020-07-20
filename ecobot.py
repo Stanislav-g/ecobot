bc30d84dc1063cb03ecdbf79be832a707e5760c9
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-20 16:17:16 +0200

    Update ecobot.py

--- ecobot.py
+++ ecobot.py
@@ -93,7 +93,7 @@ async def balance(ctx, member: discord.Member = None):
 
 @client.event
 async def on_message(message):
-    cursor.execute("UPDATE users SET xp = xp + {} WHERE id = {}".format(len(message), member.id))
+    cursor.execute("UPDATE users SET xp = xp + {} WHERE id = {}".format(1, message.author.id))
     if xp == 10:
         cursor.execute("UPDATE users SET lvl = lvl + {1} WHERE id = {}".format( member.id))
         
