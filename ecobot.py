b3ebe8170415424a176fe43283460beea8f581af
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-26 11:31:31 +0200

    Update ecobot.py

--- ecobot.py
+++ ecobot.py
@@ -505,6 +505,8 @@ async def on_raw_reaction_remove(payload):
             role = guild.get_role(736183523169534002)
         elif str(payload.emoji) == '🦅':
             role = guild.get_role(736183416885870614)
+        elif str(payload.emoji) == '🟦':
+            role = guild.get_role(736875237303386143)
     
         if role:
             member = guild.get_member(payload.user_id)
