67bb610eb3259b6aeea1d7dba0ba099892947b87
Stanislav-g <65065137+Stanislav-g@users.noreply.github.com>
2020-07-27 12:46:09 +0200

    Update ecobot.py

--- ecobot.py
+++ ecobot.py
@@ -460,23 +460,7 @@ async def reps(ctx, member: discord.Member = None):
         
 
 
-@client.event
-async def on_raw_reaction_add(payload):
-    if payload.message_id == 729977336136728637: # ID Сообщения
-        guild = client.get_guild(payload.guild_id)
-        role = None
 
-        if str(payload.emoji) == '💵': # Emoji для реакций
-            role = guild.get_role(728595813663506467) # ID Ролей для выдачи
-        
-        if role:
-            member = guild.get_member(payload.user_id)
-            if member:
-                await member.add_roles(role) 
-                cursor.execute("UPDATE users SET cash = cash + 100 WHERE id = {}".format(member.id))
-                await asyncio.sleep(60)
-                await member.remove_roles(role) 
-                connection.commit()
  
  
  
